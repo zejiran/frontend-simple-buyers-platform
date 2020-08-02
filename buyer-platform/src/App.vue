4041438c70acfaa3a99012f0ca02667ecbca78aa
zejiran <juanszalegria@gmail.com>
2020-08-01 20:50:16 -0500

    feat: add router
    Prepare project with a router and navbars for start working.

--- buyer-platform/src/App.vue
+++ buyer-platform/src/App.vue
@@ -1,60 +1,55 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+    <v-app>
+        <v-app-bar app dark color="#960200">
+            <div class="d-flex align-center">
+                <v-img
+                        alt="Vuetify Logo"
+                        class="shrink mr-2"
+                        contain
+                        src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+                        transition="scale-transition"
+                        width="40"
+                />
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+                <v-img
+                        alt="Vuetify Name"
+                        class="shrink mt-1 hidden-sm-and-down"
+                        contain
+                        min-width="100"
+                        src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+                        width="100"
+                />
+            </div>
 
-      <v-spacer></v-spacer>
+            <v-spacer></v-spacer>
+        </v-app-bar>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
+        <v-content>
+            <router-view></router-view>
+            <!--            color="#202C48"-->
+        </v-content>
 
-    <v-main>
-      <HelloWorld/>
-    </v-main>
-  </v-app>
+        <v-app-bar app dark color="#960200" bottom>
+            <v-spacer></v-spacer>
+
+            <v-btn
+                    href="https://github.com/vuetifyjs/vuetify/releases/latest"
+                    target="_blank"
+                    text
+            >
+                <span class="mr-2">Latest Release</span>
+                <v-icon>mdi-open-in-new</v-icon>
+            </v-btn>
+        </v-app-bar>
+    </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+    export default {
+        name: 'App',
 
-export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
-};
+        data: () => ({
+            //
+        }),
+    };
 </script>
--- buyer-platform/src/views/Home.vue
+++ buyer-platform/src/views/Home.vue
@@ -1,18 +1,11 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+        <h1>Homepage</h1>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'Home',
+    }
 </script>
