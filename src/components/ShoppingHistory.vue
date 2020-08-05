c6e31f6097ac68d28f9b1264b998a25f54f09b40
zejiran <juanszalegria@gmail.com>
2020-08-05 16:57:23 -0500

    feat: connect shopping history to backend
    Make get request using Axios for showing shopping history of user.

--- src/components/ShoppingHistory.vue
+++ src/components/ShoppingHistory.vue
@@ -5,9 +5,10 @@
             <v-card class="mr-10" elevation="8" min-width="200">
                 <div class="d-flex justify-space-between">
                     <div>
-                        <v-card-title class="headline" v-text="'Transaction ID: ' + transaction.id"></v-card-title>
+                        <v-card-title class="headline" v-text="'Transaction ID: ' + transaction.name"></v-card-title>
                         <v-card-subtitle v-text="'From a ' + transaction.device + ' device.'"></v-card-subtitle>
-                        <v-card-text v-text="'List of purchased products: ' + transaction.products + '.'"></v-card-text>
+                        <v-card-text
+                                v-text="'List of purchased products: ' + transaction.products + '.'"></v-card-text>
                     </div>
                 </div>
             </v-card>
@@ -16,23 +17,50 @@
 </template>
 
 <script>
+    import axios from 'axios'
+
     export default {
         data() {
             return {
                 // Test data if you don't want to create database
                 transactions: [
                     {
-                        id: '952175',
+                        name: '952175',
                         device: 'linux',
-                        products: ['Going Merry', 'Icecream', 'Rock and Stone'].join(', '),
+                        products: [{"name": "Going Merry", "price": 4423334, "id": "1342136bf"}, {
+                            "name": "Geuse",
+                            "price": 434,
+                            "id": "114567f"
+                        }]
                     },
                     {
-                        id: '234275',
+                        name: '234275',
                         device: 'mac',
-                        products: 'Excalibur',
+                        products: [{"name": "Veetee", "price": 4334, "id": "15c136bf"}],
                     },
                 ],
             }
+        },
+        mounted() {
+            const queryString = window.location.search;
+            const urlParams = new URLSearchParams(queryString);
+            const userId = urlParams.get('id')
+            // Get products by transaction id and insert on JSON
+            function products_list(transactions) {
+                for (let i = 0; i < transactions.length; i++) {
+                    let transaction = transactions[i];
+                    axios.get('http://localhost:3717/transaction/products/' + transaction.name).then(response => {
+                        transaction.products = response.data.slice(1, -2)
+                    })
+                }
+                console.log(transactions)
+            }
+            // Get transactions by user id
+            axios.get('http://localhost:3717/transaction/data/' + userId).then(response => {
+                this.transactions = JSON.parse(response.data.slice(0, -2) + "]")
+            }).then(() => {
+                products_list(this.transactions)
+            });
         }
     }
 </script>
